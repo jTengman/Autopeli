db53cc61ea4755b3e0b4764be3f453ac1b956400
rikusjoroos <riku.sjoroos@outlook.com>
2023-04-27 14:05:23 +0300

    teltta ja törmäys lisätty

---
+++ Autopeli/Assets/Tent stuff/TentCollision.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentCollision : MonoBehaviour
+{
+    public AudioSource CrashSound;
+    public int cubesPerAxis = 5;
+    public float force = 300f;
+    public float radius = 2f;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PickUp_Damaged")
+        {
+            Main();
+            Instantiate(CrashSound, transform.position, transform.rotation);
+            //gameObject.SetActive(false);
+
+        }
+
+    }
+    void Main()
+    {
+        for (int x = 0; x < cubesPerAxis; x++)
+        {
+            for (int y = 0; y < cubesPerAxis; y++)
+            {
+                for (int z = 0; z < cubesPerAxis; z++)
+                {
+                    CreateCube(new Vector3(x, y, z));
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+
+    void CreateCube(Vector3 coordinates)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Renderer rd = cube.GetComponent<Renderer>();
+        rd.material = GetComponent<Renderer>().material;
+        cube.transform.localScale = transform.localScale / cubesPerAxis;
+
+        Vector3 firstCube = transform.position - transform.localScale / cubesPerAxis;
+
+        cube.transform.position = firstCube + Vector3.Scale(coordinates, cube.transform.localScale);
+
+        Rigidbody rb = cube.AddComponent<Rigidbody>();
+        rb.AddExplosionForce(force, transform.position, radius);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
